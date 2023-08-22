bab9d5b22eed35a5e671eb9fb00b9070141920df
YusukeKawanishi <129970700+kawanishi2023@users.noreply.github.com>
2023-08-22 06:06:05 +0000

    アバウトページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   root to: "homes#top"
   # devise_for :admins
   # devise_for :users
+  get 'about' => 'homes#about'
   
   devise_for :users, skip: [:passwords], controllers: {
     registrations: "user/registrations",
